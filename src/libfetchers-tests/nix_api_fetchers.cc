576092650b3de19141f2af6503f4f0bebbf1f918
Taeer Bar-Yam <taeer@bar-yam.me>
2025-08-21 19:05:16 -0400

    disable everything to do with the C api
    - Much more thought needs to go into any changes to the C API code
    - There's a much smaller amount of code to go through
    
    As a result, it makes sense to do this once later when we see how the
    structure of things ultimately shakes out, rather than at every step

--- src/libfetchers-tests/nix_api_fetchers.cc
+++ src/libfetchers-tests/nix_api_fetchers.cc
@@ -4,15 +4,15 @@
 #include "nix_api_fetchers.h"
 #include "nix/store/tests/nix_api_store.hh"
 
-namespace nixC {
+// namespace nixC {
 
-TEST_F(nix_api_store_test, nix_api_fetchers_new_free)
-{
-    nix_fetchers_settings * settings = nix_fetchers_settings_new(ctx);
-    assert_ctx_ok();
-    ASSERT_NE(nullptr, settings);
+// TEST_F(nix_api_store_test, nix_api_fetchers_new_free)
+// {
+//     nix_fetchers_settings * settings = nix_fetchers_settings_new(ctx);
+//     assert_ctx_ok();
+//     ASSERT_NE(nullptr, settings);
 
-    nix_fetchers_settings_free(settings);
-}
+//     nix_fetchers_settings_free(settings);
+// }
 
-} // namespace nixC
+// } // namespace nixC
--- src/libmain-c/nix_api_main.cc
+++ src/libmain-c/nix_api_main.cc
@@ -5,12 +5,12 @@
 
 #include "nix/main/plugin.hh"
 
-nix_err nix_init_plugins(nix_c_context * context)
-{
-    if (context)
-        context->last_err_code = NIX_OK;
-    try {
-        nix::initPlugins();
-    }
-    NIXC_CATCH_ERRS
-}
+// nix_err nix_init_plugins(nix_c_context * context)
+// {
+//     if (context)
+//         context->last_err_code = NIX_OK;
+//     try {
+//         nix::initPlugins();
+//     }
+//     NIXC_CATCH_ERRS
+// }
