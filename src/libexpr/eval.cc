48517d2f021eabd5e97c63143964a9f373480169
Taeer Bar-Yam <taeer@bar-yam.me>
2025-08-21 16:50:22 -0400

    correctly reserve capacity for values
    before i was initializing the vector with 100000 blank entries, and any
    further elements would be added on top of that. presumably it was still
    working becauuse if you initialize the vector like that it reserves more
    capacity than just the 100000. We could probably get away with way less
    reserved, but it doesn't seem necessary for testing

--- src/libexpr/eval.cc
+++ src/libexpr/eval.cc
@@ -345,7 +345,7 @@ EvalState::EvalState(
     , trylevel(0)
     , regexCache(makeRegexCache())
 #if NIX_USE_BOEHMGC
-    , values(100000)
+    // , values(100000)
     // , valueAllocCache(std::allocate_shared<void *>(traceable_allocator<void *>(), nullptr))
     , env1AllocCache(std::allocate_shared<void *>(traceable_allocator<void *>(), nullptr))
     , baseEnvP(std::allocate_shared<Env *>(traceable_allocator<Env *>(), &allocEnv(BASE_ENV_SIZE)))
@@ -355,6 +355,7 @@ EvalState::EvalState(
 #endif
     , staticBaseEnv{std::make_shared<StaticEnv>(nullptr, nullptr)}
 {
+    values.reserve(100000);
     corepkgsFS->setPathDisplay("<nix", ">");
     internalFS->setPathDisplay("«nix-internal»", "");
 
