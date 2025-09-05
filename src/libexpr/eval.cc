35128068bff908239ae1244c9673af61f336be9a
Taeer Bar-Yam <taeer@bar-yam.me>
2025-09-04 20:37:10 -0400

    remove pre-allocation of values!

--- src/libexpr/eval.cc
+++ src/libexpr/eval.cc
@@ -402,7 +402,6 @@ EvalState::EvalState(
     char onStack;
     stackPtr = (size_t) &onStack;
 
-    values.reserve(100000000);
     corepkgsFS->setPathDisplay("<nix", ">");
     internalFS->setPathDisplay("«nix-internal»", "");
 
